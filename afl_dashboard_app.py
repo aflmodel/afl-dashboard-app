8c1ae2c9b803f0cee1caaf9c33f713af0bdc4a5c
Bill Thompson <Billycardinaels@hotmail.com>
2025-07-30 11:17:57 +1000

    r21 v1

--- afl_dashboard_app.py
+++ afl_dashboard_app.py
@@ -189,7 +189,7 @@ for sheet in sheet_names:
             home, away = [x.strip() for x in gm.split("VS")]
 
             game_info_mapping[gm] = {
-    "round": 20,
+    "round": 21,
     "home": home,
     "away": away,
     "date": pd.to_datetime(d).date() if pd.notnull(d) else None,
